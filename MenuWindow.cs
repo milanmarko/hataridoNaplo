bfed9168e4dd212fe8726fbd3c7f954134310c7d
milanmarko <marko.milan@students.jedlik.eu>
2023-12-12 13:52:01 +0100

    MenuWindow class elkezdve

---
+++ MenuWindow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hataridoNaplo
+{
+    internal class MenuWindow
+    {
+        private List<string> MenuPoints;
+        private int SelectedIndex;
+        public MenuWindow(List<string> menuPoints)
+        {
+            SelectedIndex = 0;
+            MenuPoints = menuPoints;
+        }
+
+        public void PrintMenuWindowString()
+        {
+            for (int i = 0; i < MenuPoints.Count; i++)
+            {
+                if(i == SelectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                Console.WriteLine(MenuPoints[i]);
+                if(i == SelectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+            }
+        }
+    }
+}
